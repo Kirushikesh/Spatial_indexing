16862843747fee2db350ef811f9b1645a69a4411
kirushikesh <49152921+Kirushikesh@users.noreply.github.com>
2021-06-09 18:19:12 +0530

    add function to get mbb for any polygon
    still need to add more objects and add exception cases

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
+#include <sstream>
 #include "rtree.h"
 #include "opengl.h"
 
@@ -7,24 +10,54 @@ using namespace std;
 int main(int argc,char *argv[])
 {
     rtree tree;
-    element p[]={element(1,coordinate(100,130)),element(2,coordinate(130,100)),element(3,coordinate(180,200)),
-                 element(4,coordinate(160,275)),element(5,coordinate(150,300)),element(6,coordinate(200,290)),
-                 element(8,coordinate(250,280)),element(7,coordinate(220,260)),element(9,coordinate(230,175)),
-                 element(10,coordinate(300,220)),element(11,coordinate(240,120)),element(12,coordinate(255,140))};
-    for(int i=0;i<12;i++)
-        tree.insert(&p[i]);
-    //tree.deletion(&element(1,coordinate(0,0)));
-    //tree.deletion(&element(5,coordinate(4,4)));
-    tree.root->print_node();
-    //tree.root->child[0]->print_node();
-    //tree.root->child[1]->print_node();
-    //tree.search(&element(5,coordinate(4,5)));
-    //mbb *m=new mbb(4,3,5,7);
-    //tree.knn(3,&coordinate(4,6));
-    //tree.range_search(&mbb(0,0,2,4));
+    element *e;
+    coordinate *c;
+    int key,nop,p1,p2;
+
+    fstream fio;
+    string line,word;
+    fio.open("D:/packages/3rd sem/spatial_indexing/dataset.csv",ios::in);
+    getline(fio,line);
+    vector<string> row;
+
+    stringstream geek;
+    while(fio)
+    {
+        row.clear();
+
+        getline(fio,line);
+        stringstream s(line);
+        if(line=="")
+            break;
+
+        while(getline(s, word, ','))
+        {
+            row.push_back(word);
+        }
+
+        geek << row[0];
+        geek >> key;
+        geek.clear();
+
+        geek << row[2];
+        geek >> nop;
+        geek.clear();
+
+        geek << row[3];
+        geek >> p1;
+        geek.clear();
+
+        geek <<row[4];
+        geek >> p2;
+        geek.clear();
+
+        cout<<key<<" "<<nop<<" "<<p1<<" "<<p2<<endl;
+        c=new coordinate(p1,p2);
+        e=new element(key,*c);
+        tree.insert(e);
+    }
+
+    fio.close();
     Visualize_tree(tree.root,argc,argv);
-
-    return 1;
+    return 0;
 }
-
-// Add OpenGL for visualization.
